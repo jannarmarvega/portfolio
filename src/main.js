994d6586d2764b7364b587a2abe451806fad7ff8
jannvega102 <Jannarmarvega@gmail.com>
2026-08-10 14:16:23 +0800

    fix: convert hardcoded dark SCSS colors to DaisyUI CSS custom properties

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import './assets/styles/tailwind.css'
 import './assets/styles/global.scss'
+import './assets/styles/tailwind.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
